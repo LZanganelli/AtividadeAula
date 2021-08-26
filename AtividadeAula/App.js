c100cc72828ccf85c26c54f99f5d2505411fd625
Lucas Zanganelli <lucasfzanganelli@gmail.com>
2021-08-26 00:07:26 -0300

    Update App.js
    Criacao da pagina principal e dos botões

--- AtividadeAula/App.js
+++ AtividadeAula/App.js
@@ -1,21 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const frases = ['Siga os bons e aprenda com eles.',
+  'O bom-senso vale mais do que muito conhecimento.',
+  'O riso é a menor distância entre duasssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss pessoas.',
+  'Deixe de lado as preocupaçsssssssões e seja feliz.',
+  'Realize o óbvio, pense no improvável e conquiste o impossível.',
+  'Acredite em milagres, mas não dependa deles.',
+  'A maior barreira para o sucesso é o medo do fracasso.',
+  'Tomar uma atitude é o primeiro passo para dar vida ao sonho.'
+];
+
+const [image,setImage] = useState(require('./images/biscoito.png'));
+const [text,setText] = useState('');
+
+function gerarIntAleatorio(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+  const quebrarBiscoito = () => {
+    setText('"' + frases[gerarIntAleatorio(1,frases.length)]);
+    setImage(require('./images/biscoitoQuebrado.png'));
+  }
+
+  const resetarBiscoito = () => {
+    setText('');
+    setImage(require('./images/biscoito.png'));
+  }
+
   return (
+    <SafeAreaView>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image style={styles.image} source={image}/>
+      <Image source={require('./images/biscoito.png')} />
+      <Text style={styles.text}>{text}</Text>
+      <Button color='green' mode="container" onPress={quebrarBiscoito} title="Quebrar Biscoito"></Button>
+      <Button color='blue' style={styles.button} mode="contained" onPress={resetarBiscoito} title="Tentar Novamente"></Button>
     </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    paddingTop:100,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'pink',
   },
+  image:{
+    width: 300, height: 430
+  },
+  text:{
+    fontSize:25,
+    alignContent:'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    
+  }
 });
